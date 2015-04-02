0cce3bebac20a5a095d538ec7c8cf68441065ec7
CK Hicks <ck@40digits.com>
2015-04-02 09:36:11 -0500

    adding html watch task

--- _gulp/config.js
+++ _gulp/config.js
@@ -12,7 +12,8 @@ var _source = {
 	styles:   sourceDir + '/sass/**/*.{sass,scss}',
 	symbols:  sourceDir + '/symbols/*.svg',
 	tpl:      sourceDir + '/templates/',
-	php:      appDir + '**/*.php'
+	php:      appDir + '**/*.php',
+	html:      appDir + '**/*.html'
 };
 
 // Assets Directory
@@ -99,6 +100,9 @@ module.exports = {
 	},
 	php: {
 		src: _source.php
+	},
+	html: {
+		src: _source.html
 	},
 	watch: {
 		src: _source.root,
