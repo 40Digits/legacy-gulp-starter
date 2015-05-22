10f50bbe5322778431e604499100e0ef5d5c3345
Grant Holle <grant@40digits.com>
2015-05-22 13:57:13 +0800

    Fixing wording on js config tip

--- _src/js/config/configMain.js
+++ _src/js/config/configMain.js
@@ -3,7 +3,7 @@ module.exports = {
 	jQuery selectors that include listed files when found on current page.
 
 	TIPS:
-		- Start all files with ./ to indicate their path relative to this file.
+		- Start all files with ./ to indicate their path relative to main.js.
 		- Leave off the file extension.
 		- Overqualify your selectors as much as you can for performance optimization!
 		- Prepending a ! in front of the name will make sure the file is not included
