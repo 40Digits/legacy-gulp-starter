c53dbde96ebaa5abe64c1ab160f771ecc90aab7b
Chad Watson <chad@40digits.com>
2015-06-23 11:21:24 -0500

    Reorganizes the app structure to make more sense as an npm module

--- index.js
+++ index.js
@@ -14,4 +14,4 @@
 var requireDir = require('require-dir');
 
 // Require all tasks in gulp/tasks, including subfolders
-requireDir('./_gulp/tasks', { recurse: true });
+module.exports = requireDir('./lib/tasks', { recurse: true });
